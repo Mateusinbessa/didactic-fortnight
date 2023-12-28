d973b5454d6b86eb17ad5ef9a16d970ca8a15711
Mateus Bessa <mateusbessacontato@gmail.com>
2023-12-28 12:59:30 -0300

    images feature added

--- backend/src/controller/PdfController.js
+++ backend/src/controller/PdfController.js
@@ -1,34 +1,50 @@
-import puppeteer from "puppeteer";
+import path from 'node:path';
+import { readFileSync } from 'node:fs'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import puppeteer from 'puppeteer';
 
 export async function create(req, res) {
     const { html } = req.body
-
     try {
-        //launch the browser
-        const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/google-chrome',
-            args: [
-                '--disable-setuid-sandbox',
-                '--no-sandbox'
-            ]
-        });
-        //open a new blank page
-        const page = await browser.newPage()
+        //Getting the current file path
+        const currentFilePath = fileURLToPath(import.meta.url);
+
+        //Getting the current directory name 
+        const currentDir = dirname(currentFilePath);
+
+        //Getting the root of the project
+        const projectRoot = path.join(currentDir, '..', '..', '..');
+
+        //Path to frontend assets
+        const assetsPath = path.join(projectRoot, 'frontend', 'src', 'assets');
+
+        //Making the join
+        const imagePath = path.join(assetsPath, 'logo.jpg');
+
+        //Launch the browser
+        const browser = await puppeteer.launch({ headless: 'new' });
+
+        //Open a new blank page
+        const page = await browser.newPage();
+
+        const finalHTML =
+            `<img src="data:image/jpeg;base64,${readFileSync(imagePath).toString('base64')}" alt="alt text" />${html}`
 
         //Setting the content on the page
-        await page.setContent(html)
+        await page.setContent(finalHTML);
 
         //Creating the pdf
         await page.pdf({
-            path: '/backend/src/pdf/meupdf.pdf',
+            path: 'src/pdf/meupdf.pdf',
             format: 'A4'
-        })
+        });
 
         //Closing the browser
-        await browser.close()
+        await browser.close();
 
-        res.status(201).json({ message: "PDF Criado com sucesso!" })
+        res.status(201).json({ message: "PDF Criado com sucesso!" });
     } catch (error) {
-        res.status(500).json({ message: error.message})
+        res.status(500).json({ message: error.message });
     }
 }
